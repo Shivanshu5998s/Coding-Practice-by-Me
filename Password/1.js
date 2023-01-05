bd7305bc32ef8b80a53ef7c619732b36efe4ec6b
Shivanshu5998s <96103252+Shivanshu5998s@users.noreply.github.com>
2023-01-05 17:54:12 +0530

    Code

---
+++ Password/1.js
@@ -0,0 +1,54 @@
+// // // Online Javascript Editor for free
+// // // Write, Edit and Run your Javascript code using JS Online Compiler
+
+// // // console.log("Welcome to Programiz!");
+
+
+var arr = ["abc", "dsa", "kiu"]
+
+for(i=0; i<arr.length; i++){
+    // console.log(arr[i]);
+    for(j=0; j<arr[i].length[i]; i++){
+        if(arr[i][i] === "a") {
+            continue;
+        }
+        else{
+            console.log(arr)
+        }
+        
+        // continue;
+        // console.log(arr)
+
+        // console.log(0)
+
+    }
+//     // console.log(arr)
+}
+
+// // console.log(arr)
+
+
+
+
+
+
+
+// // var newString = arr.replace("c", "")
+
+// // console.log(newString)
+
+
+
+
+
+
+
+
+
+
+
+var str = "abc ABC 123 abc";
+var strNew = str.replace("a", "");
+
+console.log(strNew);
+
